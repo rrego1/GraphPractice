37d14f012cba042cfb42b217880844a0af53ca88
reggs <reggs@LAPTOP-N9Q252NT>
2017-10-09 22:02:05 -0400

    Create an adjacency matrix from a file

--- GraphPractice/src/graphpractice/GraphPractice.java
+++ GraphPractice/src/graphpractice/GraphPractice.java
@@ -8,6 +8,8 @@ package graphpractice;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,36 +20,33 @@ public class GraphPractice {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         
-        File file = new File("graph.txt");
-        Scanner inputFile = new Scanner(file);
-        int i = inputFile.nextInt();
-        System.out.println(i);
-        //int [][] graph = createGraphMatrix("graph.txt");
+        int [][] graph = createAdjacencyMatrix("graph.txt");
         
-    }
-        
-    //takes an input file and creates an adjacency matrix
-    //the first line specifies the amount of verticies
-    //the second line specifies the number of edges
-    //following lines represent edges
-    private static int[][] createGraphMatrix(String filename) throws FileNotFoundException{
-            File file = new File(filename);
-            Scanner inputFile = new Scanner(file);
-            int verticies = inputFile.nextInt();
-            int edges = inputFile.nextInt();
-            int [][] graph = new int[verticies][verticies];
-           
-            for(int i = 0; i < edges; i++){
-                int v1 = inputFile.nextInt();
-                int v2 = inputFile.nextInt();
-                graph[v1 - 1][v2 - 1] = 1;
-                graph[v2 - 1][v1 - 1] = 1;
-            }
-            
-            return graph;
     }
     
-
+    //creates an undirect graph from a file in the form of an adjacency matrix
+    //the first line in the file represents the number of vertecies in the graph
+    //the second line represents the number of edges
+    //the following lines represent each
+    public static int[][] createAdjacencyMatrix(String filename){
+        File file = new File(filename);
+        int [][] adjacencyMatrix = null;
+        try {
+            Scanner reader = new Scanner(file);
+            int verticies = reader.nextInt();
+            int edges = reader.nextInt();
+            adjacencyMatrix = new int[verticies][verticies];
+            for(int i = 0; i < edges; i++){
+                int firstVertex = reader.nextInt();
+                int secondVertex = reader.nextInt();
+                adjacencyMatrix[firstVertex - 1][secondVertex - 1] = 1;
+                adjacencyMatrix[secondVertex - 1][firstVertex - 1] = 1;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GraphPractice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return adjacencyMatrix;
+    }
 }
